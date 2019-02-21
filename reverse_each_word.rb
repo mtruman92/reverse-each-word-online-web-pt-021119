17926959452d0c6fd03db03d8d70ab2498213ba3
Mickae Truman <mtruman@nyit.edu>
2019-02-21 03:07:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,3 +8,14 @@ new_array.join(" ")
 end
 
 reverse_each_word("Hello there, and how are you?")
+
+def reverse_each_word(sentence)
+original_array = sentence.split(" ")
+new_array = []
+original_array.collect do|sentence|
+  new_array << sentence.reverse
+end
+new_array.join(" ")
+end
+
+reverse_each_word("Hello there, and how are you?")
