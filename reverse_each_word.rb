4f3b9c9d6c9a7bf13f23762e65f42ee298196e15
Mickae Truman <mtruman@nyit.edu>
2019-02-21 03:01:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word(sentence)
-original_array = sentence.split("")
+def reverse_each_word(string)
+original_array = string.split("")
 new_array = []
-original_array.each do|sentence|
-new_array << sentence.reverse
+original_array.each do|string|
+new_array << string.reverse
 end
 new_array.join("")
 end
